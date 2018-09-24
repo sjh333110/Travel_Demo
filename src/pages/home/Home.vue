c7811a80b015d856c8bd1179b2e6072d0e781296
songjianhua <sjh333110@163.com>
2018-09-24 18:56:33 +0800

    city-keepalive

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -14,6 +14,7 @@ import HomeSwiper from './components/Swiper'
 import HomeIcons from './components/Icons'
 import HomeRecommend from './components/Recommend'
 import HomeWeekend from './components/Weekend'
+import {mapState} from 'vuex'
 import axios from 'axios'
 export default {
     name: 'Home',
@@ -26,18 +27,29 @@ export default {
     },
     data () {
         return {
+            lastCity: '',
             swiperList: [],
             iocnsList: [],
             recommendList: [],
             weekendList: [],
         }
     },
+    computed: {
+        ...mapState(['city'])
+    },
     mounted () {
+        this.lastCity = this.city;
         this.getHomeInfo();
+    },
+    activated () {
+        if (this.lastCity !== this.city) {
+            this.lastCity = this.city;
+            this.getHomeInfo();
+        }
     },
     methods : {
         getHomeInfo () {
-            axios.get('/api/index.json')
+            axios.get('/api/index.json?city='+this.city)
                 .then(this.getHomeInfoSucc)
         },
         getHomeInfoSucc (res) {
