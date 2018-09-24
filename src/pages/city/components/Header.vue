487d12a52cc0c15b3d7c1aabbf3101aae141cc2b
songjianhua <sjh333110@163.com>
2018-09-24 13:14:40 +0800

    city-router

---
+++ src/pages/city/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="header">
+        城市选择
+        <router-link to="./">
+            <div class="iconfont Header-icon">&#xe624;</div>
+        </router-link>
+        
+    </div>
+    
+</template>
+<script>
+    export default {
+        name: 'CityHeader'
+    }
+</script>
+<style lang="stylus" scoped>
+    @import '~styles/variables.styl';
+    .header
+        position : relative
+        height : $headerHeight
+        line-height : $headerHeight
+        overflow : hidden
+        text-align : center
+        background : $bgColor
+        color : #ffffff
+        font-size : .32rem
+        .Header-icon
+            position : absolute
+            top : 0
+            left : 0
+            width : .64rem
+            text-align : center
+            font-size : .4rem
+            color : #ffffff
+</style>
--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -7,10 +7,13 @@
             <span class="iconfont">&#xe632;</span>
             输入城市/景点/游玩主题
         </div>
-        <div class="header-right">
-            {{city}}
-            <span class="iconfont arrow-icon">&#xe64a;</span>
-        </div>
+        <router-link to="/city">
+            <div class="header-right">
+                {{city}}
+                <span class="iconfont arrow-icon">&#xe64a;</span>
+            </div> 
+        </router-link>
+        
     </div>
 </template>
 
@@ -27,7 +30,7 @@ export default {
     @import '~styles/variables.styl'
     .header
         display : flex
-        line-height : .86rem
+        line-height : $headerHeight
         background-color : $bgColor
         color : #ffffff
         .header-left
@@ -50,6 +53,7 @@ export default {
             width : 1.25rem
             float : right
             text-align : center
+            color : #ffffff
             .arrow-icon
                 font-size : .24rem
 </style>
